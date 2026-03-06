83308ed3880fcdcc5b965b45fe34362dbfcf2c02
kayymarklearn <kayymark.learn+github@gmail.com>
2026-03-06 20:23:04 +0000

    made all styling with decorators

--- higher-lower/server.py
+++ higher-lower/server.py
@@ -3,22 +3,35 @@ import random
 app = Flask(__name__)
 
 random_number = random.randint(0, 9)
+def add_css(func):
+    def style(*args, **kwargs):
+        number = kwargs.get('number')
+        if number == random_number:
+            return f"<div style='color: green'>{func(number)}" \
+            "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/> </div>"
+        elif number < random_number:
+            return f"<div style='color: red'>{func(number)}" \
+                "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/> </div>"
+        else:
+            return f"<div style='color: purple'>{func(number)}" \
+            "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' /> </div>"
+
+    return style
+
 @app.route('/')
 def index():
     return f"<h1>Guess a number between 0 and 9</h1>" \
     "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/> "
 
 @app.route("/<int:number>")
+@add_css
 def guessed_number(number):
     if number == random_number:
-        return f"<h1 style='color: green'>You found me!</h1>" \
-        "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+        return f"<h1>You found me!</h1>"
     elif number < random_number:
-        return f"<h1 style='color: red'>Too low, try again!</h1>" \
-        "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+        return f"<h1>Too low, try again!</h1>"
     else:
-        return f"<h1 style='color: purple'>Too high, try again!</h1>"\
-        "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' />"
+        return f"<h1>Too high, try again!</h1>"
 
 print(random_number)
 if __name__ == "__main__":
